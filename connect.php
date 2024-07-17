e2b8f97f86cafdb0ecbcb23716a160a822157511
reave <ae0u@outlook.com>
2024-07-17 14:55:37 +0700

    Initial commit

--- connect.php
+++ connect.php
@@ -1,18 +1,15 @@
 <?php
-// กำหนดค่าเชื่อมต่อฐานข้อมูล PostgreSQL
-$db_host = 'dpg-cqb74iuehbks73dkm78g-a.oregon-postgres.render.com';
-$db_port = '5432';
-$db_name = 'lovepotion_db';
-$db_user = 'root';
-$db_password = 'LjmX4r6w3FM21BZlOyCUmXUZuDiIaZbN';
+$servername = "localhost";
+$username = "root";
+$password = ""; // ใส่รหัสผ่านของคุณ
+$dbname = "lovepotion_db";
 
-// ทำการเชื่อมต่อฐานข้อมูล
-$conn = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name;user=$db_user;password=$db_password");
+// สร้างการเชื่อมต่อ
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// เช็คการเชื่อมต่อ
+// ตรวจสอบการเชื่อมต่อ
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-echo "Connected successfully";
 ?>
