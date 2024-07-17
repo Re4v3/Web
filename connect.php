eecfe51f4e960759f29b839a6fd77cb91824aa84
reave <ae0u@outlook.com>
2024-07-17 14:07:15 +0700

    Initial commit

--- connect.php
+++ connect.php
@@ -1,19 +1,14 @@
 <?php
-// ข้อมูลสำหรับเชื่อมต่อฐานข้อมูล PostgreSQL
-$dburl = "postgresql://root:LjmX4r6w3FM21BZlOyCUmXUZuDiIaZbN@dpg-cqb74iuehbks73dkm78g-a.oregon-postgres.render.com/lovepotion_db";
-
-// ทำการแยกข้อมูลจาก URL
-$urlParts = parse_url($dburl);
-
-$host = $urlParts['host'];
-$user = $urlParts['user'];
-$password = $urlParts['pass'];
-$dbname = ltrim($urlParts['path'], '/');
+$host = 'dpg-cqb74iuehbks73dkm78g-a.oregon-postgres.render.com';
+$dbname = 'lovepotion_db';
+$username = 'root';
+$password = 'LjmX4r6w3FM21BZlOyCUmXUZuDiIaZbN';
 
 try {
-    $db = new PDO('pgsql:host=dpg-cqb74iuehbks73dkm78g-a.oregon-postgres.render.com;dbname=lovepotion_db', 'root', 'LjmX4r6w3FM21BZlOyCUmXUZuDiIaZbN');
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
 } catch (PDOException $e) {
-    die('Connection failed: ' . $e->getMessage());
+    die("Connection failed: " . $e->getMessage());
 }
-
 ?>
