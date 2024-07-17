0579fe7506a40162108ba52d86f98ee965909881
reave <ae0u@outlook.com>
2024-07-17 17:14:54 +0700

    Initial commit

--- connect.php
+++ connect.php
@@ -1,13 +1,15 @@
 <?php
-$host = 'https://lovepotion.onrender.com'; // หรือใช้ localhost ถ้าใช้ที่ Render.com ก็ใช้ชื่อ host ของ Render.com
-$dbname = 'your_pg_database_name'; // ชื่อฐานข้อมูลของคุณ
-$user = 'your_pg_username'; // ชื่อผู้ใช้ฐานข้อมูลของคุณ
-$password = 'your_pg_password'; // รหัสผ่านฐานข้อมูลของคุณ
-$port = 'your_pg_port'; // พอร์ตของฐานข้อมูลของคุณ
+$servername = "localhost";
+$username = "root";
+$password = ""; // ใส่รหัสผ่านของคุณ
+$dbname = "lovepotion_db";
 
-$conn = new mysqli($host, $user, $password, $dbname, $port);
+// สร้างการเชื่อมต่อ
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// ตรวจสอบการเชื่อมต่อ
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
--- nev.php
+++ nev.php
@@ -7,7 +7,7 @@ if (session_status() == PHP_SESSION_NONE) {
 
 <nav class="navbar shadow navbar-expand-lg navbar-dark bg-pink">
     <div class="container px-5">
-        <a class="navbar-brand fw-bold" href="index.html">LOVEPOTION</a>
+        <a class="navbar-brand fw-bold" href="index.php">LOVEPOTION</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
